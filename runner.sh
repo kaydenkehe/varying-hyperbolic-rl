df99b0b515a592fdb4f6cfb5c0b053128bf09c60
Kayden K <>
2025-11-06 20:54:00 +0000

    some results

---
+++ runner.sh
@@ -0,0 +1,7 @@
+# python main_hydra.py agent@_global_=onpolicy/hyperbolic/ppo env@_global_=gen/bigfish
+# python main_hydra.py agent@_global_=onpolicy/hyperbolic/ppo env@_global_=gen/dodgeball
+# python main_hydra.py agent@_global_=onpolicy/hyperbolic/ppo env@_global_=gen/jumper
+# python main_hydra.py agent@_global_=onpolicy/hyperbolic/ppo env@_global_=gen/miner
+python main_hydra.py agent@_global_=onpolicy/hyperbolic/ppo env@_global_=gen/ninja
+python main_hydra.py agent@_global_=onpolicy/hyperbolic/ppo env@_global_=gen/caveflyer
+
---
+++ runner2.sh
@@ -0,0 +1,6 @@
+python main_hydra.py agent@_global_=onpolicy/ppo_sn env@_global_=gen/bigfish
+python main_hydra.py agent@_global_=onpolicy/ppo_sn env@_global_=gen/dodgeball
+python main_hydra.py agent@_global_=onpolicy/ppo_sn env@_global_=gen/jumper
+python main_hydra.py agent@_global_=onpolicy/ppo_sn env@_global_=gen/miner
+python main_hydra.py agent@_global_=onpolicy/ppo_sn env@_global_=gen/ninja
+python main_hydra.py agent@_global_=onpolicy/ppo_sn env@_global_=gen/caveflyer
